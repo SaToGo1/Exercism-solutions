519290baea2f551609cc0758dcee6f4044e0ca02
SaToGo <stoledano97@gmail.com>
2022-09-03 10:57:03 +0200

    (closures and Remembering)javascript Coordinate Transformation exercise DONE

--- javascript/coordinate-transformation/coordinate-transformation.js
+++ javascript/coordinate-transformation/coordinate-transformation.js
@@ -15,7 +15,7 @@
  *  translated coordinate pair in the form [x, y]
  */
 export function translate2d(dx, dy) {
-  throw new Error('Implement the translate2d function');
+  return (x, y) => [x+dx, y+dy];
 }
 
 /**
@@ -29,7 +29,7 @@ export function translate2d(dx, dy) {
  *  scaled coordinate pair in the form [x, y]
  */
 export function scale2d(sx, sy) {
-  throw new Error('Implement the scale2d function');
+  return (x, y) => [x*sx, y*sy];
 }
 
 /**
@@ -43,7 +43,11 @@ export function scale2d(sx, sy) {
  *  transformed coordinate pair in the form [x, y]
  */
 export function composeTransform(f, g) {
-  throw new Error('Implement the composeTransform function');
+  return (x, y) => {
+    [x,y] = f(x, y);
+    [x,y] = g(x, y);
+    return [x, y];
+  }
 }
 
 /**
@@ -56,5 +60,14 @@ export function composeTransform(f, g) {
  *  if the arguments are the same on subsequent calls, or compute a new result if they are different.
  */
 export function memoizeTransform(f) {
-  throw new Error('Implement the memoizeTransform function');
+    let x2;
+    let y2;
+    let result;
+  return (x, y) => {
+    if (x2===x && y2===y) return result;
+    x2=x;
+    y2=y;
+    result = f(x, y);
+    return result;
+  }; 
 }
