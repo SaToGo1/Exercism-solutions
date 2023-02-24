05cef2c9a7c296c9a56a7b6f1347f93eecbffe53
SaToGo <stoledano97@gmail.com>
2023-02-24 22:12:00 +0100

    Haskell exercise: Leap. iteration 2

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -1,8 +1,6 @@
 module LeapYear (isLeapYear) where
 
 isLeapYear :: Integer -> Bool
-isLeapYear year = (isDivisibleBy4 year && not (isDivisibleBy100 year)) || isDivisibleBy400 year
+isLeapYear year = (isDivisibleBy 4 year && not (isDivisibleBy 100 year)) || isDivisibleBy 400 year
 
-isDivisibleBy4 x = (mod x 4) == 0
-isDivisibleBy100 x = (mod x 100) == 0
-isDivisibleBy400 x = (mod x 400) == 0
+isDivisibleBy x year = (mod year x) == 0
