10aa564cf76783a465c7055dacad90f49a617388
SaToGo <stoledano97@gmail.com>
2023-02-25 22:01:02 +0100

    Haskell exercise: Collatz Conjecture. Second try

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,15 +1,26 @@
 module CollatzConjecture (collatz) where
 
 collatz :: Integer -> Maybe Integer
-collatz n = 
-  if n == 1
-    then n
-    else if isEven
-        then collatz evenOperation n 
-        else collatz oddOperation n
+-- collatz n = 
+--   if n == 0 then
+--     Nothing
+--     else
+--       if n == 1 then 
+--           n
+--         else 
+--           if isEven n then 
+--             collatz (evenOperation n) + 1
+--           else 
+--             collatz (oddOperation n) + 1
 
-isEven x = x mod 2 == 0
-isOdd x = x mod 2 /= 0
+collatz n 
+  | n == 0 = Nothing
+  | n == 1 = Just 0
+  | isEven n = Just (collatz ( evenOperation n ) + 1)
+  | isOdd n = collatz ( oddOperation n )
 
-evenOperation n = n / 2
+isEven x = (mod x 2) == 0
+isOdd x = (mod x 2) /= 0
+
+evenOperation n = n `div` 2
 oddOperation n = n*3 + 1
