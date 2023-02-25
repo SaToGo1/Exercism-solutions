45d85ed2cb9fce1d77adfe0ae7867c0b59bc4bc1
SaToGo <stoledano97@gmail.com>
2023-02-25 20:29:35 +0100

    Haskell exercise: Collatz Conjecture. First try

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,4 +1,15 @@
 module CollatzConjecture (collatz) where
 
 collatz :: Integer -> Maybe Integer
-collatz n = error "You need to implement this function."
+collatz n = 
+  if n == 1
+    then n
+    else if isEven
+        then collatz evenOperation n 
+        else collatz oddOperation n
+
+isEven x = x mod 2 == 0
+isOdd x = x mod 2 /= 0
+
+evenOperation n = n / 2
+oddOperation n = n*3 + 1
