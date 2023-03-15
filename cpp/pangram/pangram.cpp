2cd7946c43168bb46c0041e168bd49b90321d04c
SaToGo <stoledano97@gmail.com>
2023-03-15 01:43:27 +0100

    C++ exercise: 'Pangram'. Working.

--- cpp/pangram/pangram.cpp
+++ cpp/pangram/pangram.cpp
@@ -1,5 +1,20 @@
 #include "pangram.h"
 
 namespace pangram {
+    bool is_pangram(str){
+        char letter = 'a';
+        char LETTER = 'A';
+        bool isPangram = true;
+        for(int i = 0; i < 26; i++){
+            bool containsLetter = false;
+            for(int j = 0; j < str.size(); j++){
+                if( (str[j] == letter) or (str[j] == LETTER) ){
+                    containsLetter = true;
+                }
+            }
+            isPangram = (isPangram && containsLetter);
+        }
 
+        return isPangram;
+    }
 }  // namespace pangram
--- cpp/pangram/pangram.h
+++ cpp/pangram/pangram.h
@@ -2,7 +2,7 @@
 #define PANGRAM_H
 
 namespace pangram {
-
+bool is_pangram(string);
 }  // namespace pangram
 
 #endif // PANGRAM_H
