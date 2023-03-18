dada15c2d19b53539edf9971379b597ae1a4b2db
SaToGo <stoledano97@gmail.com>
2023-03-18 20:27:15 +0100

    C++ Reverse String fourth iteration of the solution

--- cpp/reverse-string/reverse_string.cpp
+++ cpp/reverse-string/reverse_string.cpp
@@ -1,20 +1,10 @@
 #include "reverse_string.h"
 
+#include <algorithm>
+
 namespace reverse_string {
     std::string reverse_string(std::string input){
-        if(input.empty()) {
-            return input;
-        } 
-        
-        size_t left = 0;
-        size_t right = input.length() - 1; //unsigned value, can't have negative number
-
-        while(left < right){
-            std::swap(input[left], input[right]);
-            left++;
-            right--;
-        }
-
+        std::reverse(input.begin(), input.end());
         return input;
     }
 }  // namespace reverse_string
