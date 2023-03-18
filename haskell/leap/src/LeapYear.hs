7a8ab7e7d6c7c56667683d177e966192c9ac0e6b
SaToGo <stoledano97@gmail.com>
2023-03-18 18:49:56 +0100

    Cleaning Haskell Exercises

--- haskell/leap/src/LeapYear.hs
+++
@@ -1,6 +0,0 @@
-module LeapYear (isLeapYear) where
-
-isLeapYear :: Integer -> Bool
-isLeapYear year = (isDivisibleBy 4 year && not (isDivisibleBy 100 year)) || isDivisibleBy 400 year
-
-isDivisibleBy x year = (mod year x) == 0
