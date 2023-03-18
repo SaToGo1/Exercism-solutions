bf5e65d30ef8f31d275e8a9da0c2b2727f519ffb
SaToGo <stoledano97@gmail.com>
2023-03-18 20:12:20 +0100

    C++ reverse string first solution

--- cpp/reverse-string/reverse_string.cpp
+++ cpp/reverse-string/reverse_string.cpp
@@ -2,6 +2,13 @@
 
 namespace reverse_string {
     std::string reverse_string(std::string input){
-        return input;
+        std::string reversed;
+        int length = input.length();
+
+        for(int i = length - 1; i >= 0; i--){
+            reversed += input[i];
+        }
+
+        return reversed;
     }
 }  // namespace reverse_string
